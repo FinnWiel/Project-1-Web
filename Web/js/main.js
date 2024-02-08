747cd531f1afc5e01cc52feeaaca54d7f465e9d2
Finn van de Wiel <finnvandewiel01@gmail.com>
2024-02-08 19:31:16 +0100

    dynamic year copyright

--- Web/js/main.js
+++ Web/js/main.js
@@ -1,3 +1,13 @@
+//Gets the current year for footer
+
+
+window.addEventListener("load", (event) => {
+    let copyright = document.getElementById("year");
+    let currentYear = new Date().getFullYear();
+    copyright.innerHTML = currentYear;
+});
+
+//Toggles mobile nav menu
 function toggleMobileNav(){
     let menu = document.getElementById("navigation");
     let bars = document.querySelectorAll('.bar')
