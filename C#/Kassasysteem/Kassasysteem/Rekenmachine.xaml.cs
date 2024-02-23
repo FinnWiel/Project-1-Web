8068eb7c91f4dd42160c9dbc9a902014c95f88d4
Finn van de Wiel <finnvandewiel01@gmail.com>
2024-02-23 23:35:44 +0100

    calculator

--- C#/Kassasysteem/Kassasysteem/Rekenmachine.xaml.cs
+++ C#/Kassasysteem/Kassasysteem/Rekenmachine.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -24,24 +25,51 @@ namespace Kassasysteem
         public Rekenmachine()
         {
             InitializeComponent();
+
         }
 
         private void Button_Click_Number(object sender, RoutedEventArgs e)
         {
-            Button b = (Button)sender;
-            tbCurrentCalc.Text += b.Content.ToString();
+            Button clickedButton = (Button)sender;
+
+            switch (clickedButton.Content.ToString())
+            {
+                case "sin":
+                    tbCurrentCalc.Text += clickedButton.Content.ToString() + "(";
+                    break;
+                case "cos":
+                    tbCurrentCalc.Text += clickedButton.Content.ToString() + "(";
+                    break;
+                case "tan":
+                    tbCurrentCalc.Text += clickedButton.Content.ToString() + "(";
+                    break;
+                case "√x":
+                    tbCurrentCalc.Text += "√";
+                    break;
+                default:
+                    tbCurrentCalc.Text += clickedButton.Content.ToString();
+                    break;
+            }
         }
 
         private void Calculate(object sender, RoutedEventArgs e)
         {
-            string calculation = tbCurrentCalc.Text;
         }
 
         private void btnCE_Click(object sender, RoutedEventArgs e)
         {
             string calculation = tbCurrentCalc.Text;
-            string newCalc = calculation.Remove(calculation.Length - 1, 1);
-            tbCurrentCalc.Text = newCalc;
+
+            if (calculation.Length > 0)
+            {
+                string newCalc = calculation.Remove(calculation.Length - 1, 1);
+                tbCurrentCalc.Text = newCalc;
+            }
+            else
+            {
+                return;
+            }
+
         }
     }
 }
