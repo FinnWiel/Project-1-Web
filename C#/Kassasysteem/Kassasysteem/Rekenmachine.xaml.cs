8f69787938fbb16a61dcf20409cfb5f4221a1d72
Finn van de Wiel <finnvandewiel01@gmail.com>
2024-02-23 22:01:41 +0100

    rekenmachine layout

---
+++ C#/Kassasysteem/Kassasysteem/Rekenmachine.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kassasysteem
+{
+    /// <summary>
+    /// Interaction logic for Rekenmachine.xaml
+    /// </summary>
+    public partial class Rekenmachine : Window
+    {
+        public Rekenmachine()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_Number(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            tbCurrentCalc.Text += b.Content.ToString();
+        }
+
+        private void Calculate(object sender, RoutedEventArgs e)
+        {
+            string calculation = tbCurrentCalc.Text;
+        }
+
+        private void btnCE_Click(object sender, RoutedEventArgs e)
+        {
+            string calculation = tbCurrentCalc.Text;
+            string newCalc = calculation.Remove(calculation.Length - 1, 1);
+            tbCurrentCalc.Text = newCalc;
+        }
+    }
+}
